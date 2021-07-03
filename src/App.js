ff2365656199af0f8b6b9b38396a291bee83b6aa
Seva <semionkrutsikau@gmail.com>
2021-07-03 21:20:30 +0300

    Tic-tac-toe

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react"
+import Game from './Game/game'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Game/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
